fa3eb977241fa8013424faa120d24757f2579ad2
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-03-03 19:07:54 +0100

    update

--- Warlord/models/GetParticipantArmour.cs
+++ Warlord/models/GetParticipantArmour.cs
@@ -16,7 +16,7 @@ namespace Warlord.models
         {
             if (CampaignMission.Current.Mode == MissionMode.Tournament)
             {
-                string text = string.Concat(new object[] { "tournament_", Settlement.CurrentSettlement.Culture.StringId });
+                string text = string.Concat(new object[] { "tournament_armour_", Settlement.CurrentSettlement.Culture.StringId });
                 return (Game.Current.ObjectManager.GetObject<CharacterObject>(text) ?? Game.Current.ObjectManager.GetObject<CharacterObject>("gear_practice_dummy_empire")).RandomBattleEquipment;
             }
             // The weapon loadout still is changed by the "tournament_template_<culture>_<amount>_participant_set_vX" NPC, but now the armour can be changed by having an NPC with the ID "tournament_<culture>"

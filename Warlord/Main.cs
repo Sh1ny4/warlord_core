71070112b5960fb56aad2664f4c6278c73183ff4
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-03-03 17:10:39 +0100

    Ajoutez des fichiers projet.

---
+++ Warlord/Main.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+
+namespace Warlord
+{
+    public class Main : MBSubModuleBase
+    {
+        /*
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("wipo.patches").PatchAll();
+        }
+
+        protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
+        {
+            if (starterObject is CampaignGameStarter)
+            {
+                CampaignGameStarter campaignGameStarter = starterObject as CampaignGameStarter;
+                campaignGameStarter.AddBehavior(new patches.EliteInCastle.CastleRecruitMenu());
+            }
+        }*/
+
+        public override void OnGameInitializationFinished(Game game)
+        {
+            if (!(game.GameType is Campaign)) return;
+            Campaign.Current.CampaignBehaviorManager.RemoveBehavior<BackstoryCampaignBehavior>();
+        }
+    }
+}

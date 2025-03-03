189d3ba6c050128eacd1e7fb08ae7df0e9b43579
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-03-03 19:37:58 +0100

    update

--- Warlord/models/PartySizeModel.cs
+++ Warlord/models/PartySizeModel.cs
@@ -1,9 +1,5 @@
 ﻿using Helpers;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem.CharacterDevelopment;
 using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.CampaignSystem.Party;

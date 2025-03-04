13c11808772de28a6d476d5c73eb297c2b4df56a
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-03-04 02:06:06 +0100

    update

--- Warlord/Main.cs
+++ Warlord/Main.cs
@@ -3,6 +3,12 @@ using TaleWorlds.Core;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using System;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade.View;
+using TaleWorlds.ScreenSystem;
+using SandBox.View;
+using SandBox;
 
 namespace Warlord
 {
@@ -12,8 +18,35 @@ namespace Warlord
         {
             base.OnSubModuleLoad();
             new Harmony("Warlord.patches").PatchAll();
+            TextObject coreContentDisabledReason = new TextObject("{=!}StoryMode Disable", null);
+            TextObject _sandBoxAchievementsHint = new TextObject("{=j09m7S2E}Achievements are disabled in SandBox mode!", null);
+            Module.CurrentModule.ClearStateOptions();
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("SandBoxNewGame", new TextObject("{=!}Start a New Game", null), 3, delegate () { MBGameManager.StartNewGame(new SandBoxGameManager()); }, () => this.IsSandboxDisabled(), _sandBoxAchievementsHint));
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("CampaignResumeGame", new TextObject("{=!}Load Game", null), 0, delegate () { ScreenManager.PushScreen(SandBoxViewCreator.CreateSaveLoadScreen(false));}, () => this.IsSavedGamesDisabled(), null));
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Options", new TextObject("{=!}Options", null), 9998, delegate () { ScreenManager.PushScreen(ViewCreator.CreateOptionsScreen(true)); }, () => new ValueTuple<bool, TextObject>(false, TextObject.Empty), null));
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Credits", new TextObject("{=!}Credits", null), 9999, delegate () { ScreenManager.PushScreen(ViewCreator.CreateCreditsScreen()); }, () => new ValueTuple<bool, TextObject>(false, TextObject.Empty), null));
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Exit", new TextObject("{=!}Exit Game", null), 10000, delegate () { MBInitialScreenBase.DoExitButtonAction(); }, () => new ValueTuple<bool, TextObject>(Module.CurrentModule.IsOnlyCoreContentEnabled, coreContentDisabledReason), null));
         }
-
+        private ValueTuple<bool, TextObject> IsSandboxDisabled()
+        {
+            if (Module.CurrentModule.IsOnlyCoreContentEnabled)
+            {
+                return new ValueTuple<bool, TextObject>(true, new TextObject("{=V8BXjyYq}Disabled during installation.", null));
+            }
+            return new ValueTuple<bool, TextObject>(false, TextObject.Empty);
+        }
+        private ValueTuple<bool, TextObject> IsSavedGamesDisabled()
+        {
+            if (Module.CurrentModule.IsOnlyCoreContentEnabled)
+            {
+                return new ValueTuple<bool, TextObject>(true, new TextObject("{=V8BXjyYq}Disabled during installation.", null));
+            }
+            if (MBSaveLoad.NumberOfCurrentSaves == 0)
+            {
+                return new ValueTuple<bool, TextObject>(true, new TextObject("{=XcVVE1mp}No saved games found.", null));
+            }
+            return new ValueTuple<bool, TextObject>(false, TextObject.Empty);
+        }
         /*
         protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
         {
